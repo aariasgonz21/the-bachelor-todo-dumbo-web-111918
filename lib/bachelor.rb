60025e89fb3be3d9a434e344017d2a38603ea72f
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2018-11-18 21:18:00 +0000

    automatically backed up by learn

---
+++ lib/bachelor.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+def get_first_name_of_season_winner(data, season)
+  first_name = " "
+  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      first_name = contestant["name"].split(" ")[0]
+      binding.pry
+  end
+  
+end
+
+def get_contestant_name(data, occupation)
+  # code here
+end
+
+def count_contestants_by_hometown(data, hometown)
+  # code here
+end
+
+def get_occupation(data, hometown)
+  # code here
+end
+
+def get_average_age_for_season(data, season)
+  # code here
+end
