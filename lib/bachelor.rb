eac4c7b7ba12c8c1bfeb3871d82cf6d82846cff8
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2018-11-18 21:27:40 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -13,7 +13,7 @@ end
 def get_contestant_name(data, occupation)
   data.each do |season, array|
     array.each do |contestant|
-      if contestant["occupation"] = occupation
+      if contestant["occupation"] == occupation
         return contestant["name"]
       end
     end
