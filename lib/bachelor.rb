ecc71983e04cb5a5d12351cb532f7d6140dea0db
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2018-11-18 21:42:52 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -43,6 +43,8 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
+  ages = 0
+  count = 0
   data[season].each do |contestant|
     contestant["age"].to_f
   end
